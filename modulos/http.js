d4759f768575349d535f04651ca8445eead3c11d
Pablo <pablogarcirodriguez@gmail.com>
2023-08-09 19:08:03 +0200

    Clase 17: HTTP

---
+++ modulos/http.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+http.createServer(router).listen(3000);
+
+function router(req, res) {
+  console.log("Nueva petición");
+  console.log(req.url);
+
+  switch (req.url) {
+    case "/hola":
+      res.write("Hola que tal");
+      res.end();
+      break;
+    default:
+      res.write("Error 404: No se lo que quieres");
+      res.end();
+  }
+
+  //   res.writeHead(201, { "Content-Type": "text/plain" });
+  //Escribir respuesta al usuario
+  //   res.write("Hola esto es HTTP  de NodeJS");
+  //   res.end();
+}
+
+console.log("Escuchando HTTP en  puerto 3000");
